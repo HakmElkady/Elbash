d33784098269fd5c6bab5ffb17aa8652940dd997
HakmElkady <ENGHAKM24@GMAIL.COM>
2023-01-04 02:39:29 +0200

    My First Commit

--- ElrahmaForms/App/Forms/Attendance/Clsattend.cs
+++ ElrahmaForms/App/Forms/Attendance/Clsattend.cs
@@ -17,7 +17,7 @@ namespace ElrahmaForms.App.Forms
 
 
         public void Get()
-        {
+        {                                                                                                                                                       ///
             string SqlCommand = "select EmpName,checkin,checkout,ID,e.empid from checkinout c inner join employees e\r\non e.empid=c.empid\r\nwhere checkout is null  ";
             Dt_Get = new DataTable();
             XclsDb.Select(SqlCommand, null, Dt_Get);
